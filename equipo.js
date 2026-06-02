b739231ec819a5247d2e56dd9aa1563d1ebbef46
Theoclas <fernando.pala.99@gmail.com>
2026-06-02 08:02:56 -0500

    js para saber name de equipo

---
+++ equipo.js
@@ -0,0 +1,16 @@
+
+
+require("dotenv").config();
+
+
+const os = require("os");
+const { url } = require("inspector");
+require('dotenv').config();
+
+const EquiposGenerales = JSON.parse(process.env.EQUIPOS_GENERALES);
+
+const NombreEquipo = os.hostname();
+console.log(" Nombre del equipo: ", NombreEquipo);
+
+const EquipoActual = EquiposGenerales[NombreEquipo];
+console.log(" Equipo Actual: ", EquipoActual);
